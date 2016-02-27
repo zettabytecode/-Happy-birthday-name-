2f9d2c657753c33fd82cc404f46b7381504b270b
Ola Marc <olamarcb@gmail.com>
2016-02-26 16:49:14 -0800

    Added files via upload

---
+++ Happy-birthday-name.py
@@ -0,0 +1,14 @@
+def happy_birthday():
+    print "What is your name?"
+    name = raw_input("Type your name and hit 'Enter'.")
+    print "Is today your birthday?"
+    answer = raw_input("Type yes or no and hit 'Enter'.").lower()
+    if answer == "yes" or answer == "y":
+        print "Happy birthday,",name + "!"
+    elif answer == "no" or answer == "n":
+        print "Have a good day,",name + "!"
+    else:
+        print "You didn't pick yes or no! Try again."
+        happy_birthday()
+
+happy_birthday()
